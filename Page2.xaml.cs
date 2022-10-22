0c0a754addc8f088b72abd6c6f4da36faca144d5
Egor <60449070+egor154@users.noreply.github.com>
2022-10-22 14:22:43 +0300

    -

--- Page2.xaml.cs
+++ Page2.xaml.cs
@@ -11,5 +11,10 @@ namespace IWT_
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            MyFrame.Content = new Page3();
+        }
     }
 }
